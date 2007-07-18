d91c931b64d21ee83375214f4833a3ce72c52852
Jon Carey <unknown@unknown.org>
2007-07-18 21:43:51 +0000

    Fixed problem with interval cimdatetime conversion. referencing microsecond. should be microseconds
    git-svn-id: https://svn.code.sf.net/p/pywbem/code/pyprovifc/openwbem/trunk@284 84f79d66-d411-0410-a85a-a3183736c6e8

--- src/ifc/pyprovider/OW_PyConverter.cpp
+++ src/ifc/pyprovider/OW_PyConverter.cpp
@@ -125,7 +125,7 @@ convertPyDateTime(
 		cdt.setInterval(true); 
 		int days = Py::Int(td.getAttr("days")).asLong();
 		int seconds = Py::Int(td.getAttr("seconds")).asLong();
-		int microseconds = Py::Int(td.getAttr("microsecond")).asLong();
+		int microseconds = Py::Int(td.getAttr("microseconds")).asLong();
 		int hours = seconds / 3600; 
 		int minutes = (seconds - hours * 3600) / 60; 
 		seconds = seconds - hours * 3600 - minutes * 60; 
